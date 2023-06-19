dc387dc16c3d418d848a62fea7cc0ac5f9628cce
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2023-06-19 21:34:48 +0400

    Update and rename DZ1.js to lesson1.js

--- DZ1/DZ1.js
+++
@@ -1,6 +0,0 @@
-let x = 6;
-let mod = x % 2; //остаток числа
-if (mod >= 0){
-    console.log('чётное');
-}
-else { console.log('нечётное'); }
---
+++ Урок 1 /lesson1.js
@@ -0,0 +1,11 @@
+//комментарий
+/*многострочный комментарий*/
+let x = 6;
+let mod = x % 2; //остаток числа
+if (mod >= 0){
+    console.log('чётное');
+}
+else { console.log('нечётное'); }
+
+color = prompt('введите цвет');
+document.body.style.background = color;//меняем цвет на страницы
