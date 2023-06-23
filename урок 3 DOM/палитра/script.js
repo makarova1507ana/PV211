7a59e167fedf014a2f8b1e1cc4d85df56134349d
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2023-06-23 20:45:46 +0400

    Add files via upload

---
+++ урок 3 DOM/палитра/script.js
@@ -0,0 +1,11 @@
+function setcolored() {
+    document.querySelector('p').style.color = 'red';
+}
+
+function setcolorblue() {
+    document.querySelector('p').style.color = 'rgb(32, 32, 163)';
+}
+
+function setcolorlightblue() {
+    document.querySelector('p').style.color = 'rgb(0, 217, 255)';
+}
