c256955ad57dfb4a6aea0a2128821dabe72dde31
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2023-07-14 21:28:42 +0400

    Add files via upload

--- search_videos/script.js
+++ search_videos/script.js
@@ -7,10 +7,13 @@
 // после получения фильмов необходимо реализовать пагинацию - (разбивка контента  на страницы)
 
 let btn_search = document.getElementById('btn_search'); // btn_search
+let results = document.getElementById('results'); // div results
+
 //    // запрос к серверу для получения json-файла 
 //http://www.omdbapi.com/?apikey=[yourkey]&
 const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=23f82659';
-
+// films were founded 5718 -> page 1 первые 10 фильмов(с 1 до 10) -> page 2 следующие 10 фильмов(с 11 до 20)
+//http://www.omdbapi.com/?i=tt3896198&apikey=23f8265&s='hello'&page=10  - выдаст с 101 по 110 фильмы
 
 function sendRequest(url) {
     return fetch(url).then(response => { return response.json() }) // fetch  возвращает promise    
@@ -18,30 +21,61 @@ function sendRequest(url) {
 
 /*GET request */
 btn_search.addEventListener('click', function() {
-        // механика поиска 
-        // t -title
-        // url   +   &t= + "hello"
-        title = document.getElementById('title').value;
-        if (title != '') {
-            requestUrl = url + '&t="' + title + '"';
-        } else {
-            requestUrl = url;
-        }
+        requestUrl = currentUrl();
         sendRequest(requestUrl)
-            .then(data => (
-                get_movie(data))) //в случае успешного выполнения кода
+            .then(data => (console.log(data), result(data)))
             .catch(err => (
                 console.log('error data', err))) //в случае неуспешного выполнения кода
     }
 
 );
 
-//    //  json-файла распарсить и отобразить на странице
-function get_movie(obj) {
-    // for (el in array) {
-    console.log(obj);
-    block_movie(obj);
+function currentUrl() {
+    // механика поиска 
+    // t -title
+    // url   +   &s= + "hello"
+    results.innerHTML = "";
+    title = document.getElementById('title').value;
+    types = document.querySelectorAll('option');
+    let Type;
+    for (type of types) {
+        if (type.selected) {
+            Type = type.value;
+            break
+        }
+    }
 
+    if (title != '') {
+        curlUrl = url + '&s="' + title + '"&type="' + Type + '"';
+    } else {
+        curlUrl = url;
+    }
+    return curlUrl
+}
+
+function result(data) {
+
+    if (data['Response'] == "True") { //true или false
+        count_results = document.createElement('h2');
+        count_results.innerHTML = "нашлось фильмов: " + data["totalResults"];
+        results.append(count_results);
+
+        get_movie(data['Search'])
+    } else {
+        /*title */
+        error = document.createElement('h3');
+        error.innerHTML = "films not found";
+        results.append(error);
+
+        console.log('films not found');
+    }
+}
+//    //  json-файла распарсить и отобразить на странице
+function get_movie(array) {
+
+    for (let i = 0; i < array.length; i++) {
+        block_movie(array[i]);
+    }
 
 }
 
@@ -50,16 +84,26 @@ function block_movie(obj) {
     /*title */
     title = document.createElement('h3');
     title.innerHTML = obj["Title"];
-    document.body.append(title);
+    results.append(title);
 
     /*poster */
     poster = document.createElement('div');
-    poster.innerHTML = '<img style="width:150px; height:300px;" src="' + obj["Poster"] + '">';
-    document.body.append(poster);
+    if (obj["Poster"] == "N/A") {
+        // нужно добавить картинку по умолчанию
+        poster.innerHTML = '<img style="width:150px; height:300px;" alt="здесь должен быть постер"';
+    } else {
+        poster.innerHTML = '<img style="width:150px; height:300px;" src="' + obj["Poster"] + '">';
+    }
+    results.append(poster);
+
+    /*type */
+    type = document.createElement('p');
+    type.innerHTML = "<strong>type: </strong>" + obj["Type"];
+    results.append(type);
 
     /*btn_deatails */
     btn_details = document.createElement('input');
     btn_details.type = "button";
     btn_details.value = "details";
-    document.body.append(btn_details);
+    results.append(btn_details);
 }
