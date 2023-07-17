eae8eaf7b640e6b18af461a473126b3f56ae9934
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2023-07-17 20:05:21 +0400

    Add files via upload

--- search_videos/script.js
+++ search_videos/script.js
@@ -11,9 +11,9 @@ let results = document.getElementById('results'); // div results
 
 //    // запрос к серверу для получения json-файла 
 //http://www.omdbapi.com/?apikey=[yourkey]&
-const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=23f82659';
+const url = 'http://www.omdbapi.com/?apikey=23f82659';
 // films were founded 5718 -> page 1 первые 10 фильмов(с 1 до 10) -> page 2 следующие 10 фильмов(с 11 до 20)
-//http://www.omdbapi.com/?i=tt3896198&apikey=23f8265&s='hello'&page=10  - выдаст с 101 по 110 фильмы
+//http://www.omdbapi.com/?apikey=23f8265&s='hello'&page=10  - выдаст с 101 по 110 фильмы
 
 function sendRequest(url) {
     return fetch(url).then(response => { return response.json() }) // fetch  возвращает promise    
@@ -79,6 +79,48 @@ function get_movie(array) {
 
 }
 
+show_details = Event => {
+    // Какая кнопка была нажата
+    id = Event.currentTarget.id;
+    // получим Json файл по запросу фильма http://www.omdbapi.com/?apikey=23f82659 +  &i= у кнопки возьмем id
+    requestUrl = url + "&i=" + id;
+    sendRequest(requestUrl)
+        .then(data => (console.log(data), block_info(id, data))) // направлять на обратку JSON (data)  и отображения результата         // функция которая отобразит все данные о фильме
+        .catch(err => (
+            console.log('error data', err))) //в случае неуспешного выполнения кода
+
+}
+
+
+//Показывает таблицу с подробной информацией о фильме
+function block_info(id, data_json) {
+    /*title */
+    btn = document.getElementById(id);
+    console.log(btn)
+    table = document.createElement('tbody');
+
+
+    //перебираем все свойства нашего файла JSON
+    for (element in data_json) {
+
+        if (data_json[element] !== 'N/A' && element !== 'imdbID' && element !== 'Response') {
+
+            //в рейтинге отдельно хранятся объекты JSON в которых есть информаация о кадом рейтинге
+            if (element == 'Ratings') {
+                table.innerHTML += "<tr>   <th>" + element + "</th>  <td>" + data_json[element][0]['Source'] + " - " + data_json[element][0]['Value'] + "</td     </tr>";
+
+                table.innerHTML += "<tr>   <td></td> <td>" + data_json[element][1]['Source'] + " - " + data_json[element][1]['Value'] + "</td     </tr>";
+                table.innerHTML += "<tr>   <td></td><td>" + data_json[element][2]['Source'] + " - " + data_json[element][2]['Value'] + "</td     </tr>";
+
+            } else {
+                table.innerHTML += "<tr>   <th>" + element + "</th>   <td>" + data_json[element] + "</td>    </tr>";
+            }
+        }
+    }
+    table.innerHTML += "</tbody>";
+    btn.after(table);
+
+}
 //create one the block of movie
 function block_movie(obj) {
     /*title */
@@ -104,6 +146,9 @@ function block_movie(obj) {
     /*btn_deatails */
     btn_details = document.createElement('input');
     btn_details.type = "button";
+    btn_details.id = obj['imdbID']; // 
     btn_details.value = "details";
     results.append(btn_details);
+    btn_details.addEventListener('click', show_details); // btn_deatails добавить обработку событий 
+
 }
