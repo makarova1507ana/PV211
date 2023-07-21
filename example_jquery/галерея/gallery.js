3f8e94a52a571bb6e0ef4b6232c129d0767106c2
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2023-07-21 18:37:22 +0400

    Add files via upload

---
+++ example_jquery/галерея/gallery.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    let images = ['image1.jpg', 'image2.jpg', 'image3.jpeg', 'image4.jpg'];
+    let currentIndex = 0;
+
+    function showImage(index) {
+        let imageUrl = images[index];
+        $(".image-container img").attr('src', imageUrl);
+    }
+
+    function navigateToNextImage() {
+        currentIndex++;
+        if (currentIndex >= images.length) {
+            currentIndex = 0;
+        }
+        showImage(currentIndex);
+    }
+
+    function navigateToPreviousImage() {
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = images.length - 1;
+        }
+        showImage(currentIndex);
+    }
+
+    $('.next-button').click(function() {
+        navigateToNextImage();
+    });
+
+    $('.prev-button').click(function() {
+        navigateToPreviousImage();
+    })
+})
